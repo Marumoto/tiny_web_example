77f5215e52c3472a9deb3d128cb7ef9500e749de
zonomasa <tatezono@gmail.com>
2015-05-30 02:53:39 -0700

    Fix for jenkins

---
+++ ciscripts/test_web.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#############################################
+# usage : ./this.sh ip_address_of_db_server
+#############################################
+
+set -eux
+set -o pipefail
+
+
+DBSERVER_IP=$1
+
+
+cd /opt/axsh/tiny-web-example/spec_integration
+bundle install
+
+cd /opt/axsh/tiny-web-example/spec_integration/config
+cp webapi.conf.example webapi.conf
+
+sed -i -e "s|localhost|${DBSERVER_IP}|g" webapi.conf
+
+
+cd /opt/axsh/tiny-web-example/spec_integration
+bundle exec rspec ./spec/webapi_integration_spec.rb
+
